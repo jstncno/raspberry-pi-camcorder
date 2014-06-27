afde189fc6d68a01ab9385e2f1f8c45b6a206e99
Justin <jcano001@ucr.edu>
2014-06-27 01:37:24 -0700

    appends timestamp to filename

--- camcorder.py
+++ camcorder.py
@@ -3,10 +3,16 @@
 # Python script to record video clips to an external mounted USB NTFS hard drive.
 # Use time.sleep() record for desired amount of time
 #
+# Video files saved as .h264 named by their timestamp
+# Filename: video_<month>-<day>-<year>_<weekday><day>_<hour><min><sec>.h264
+# Format: MM-DD-YY_DayDD_hhmmss
+# Example: 06-27-14_Thurs27_221834 => Thursday, June 27 2014 at 10:18pm
+#
 # External PiCam resources:
 # http://www.raspberrypi.org/documentation/usage/camera/python/README.md
 # http://www.raspberrypi.org/learning/python-picamera-setup/
 import time
+import datetime
 import picamera
 import RPi.GPIO as GPIO
 
@@ -14,12 +20,11 @@ MINUTE = 60 # in seconds
 HOUR = 3600 # in seconds
 
 camera = picamera.PiCamera()
-count = 0
 
 while (1): # run forever
-	count += 1
-	print "recording video", count
-	filename = '/media/usbhdd/video_%d.h264' % count
+	date = datetime.datetime.now().strftime('%m-%d-%y_%a%b%d_%H%M%S')
+	filename = '/media/usbhdd/video_' + date + '.h264'
+	print 'recording video clip', date
 	camera.start_recording(filename)
 	time.sleep(HOUR) # record for an hour
 	camera.stop_recording()
--- time-lapse.py
+++ time-lapse.py
@@ -3,10 +3,16 @@
 # Python script to capture time-lapse images to an external mounted USB NTFS hard drive.
 # Use time.sleep() for delay between frames
 # 
+# Image files saved as .jpg named by their timestamp
+# Filename: img_<month>-<day>-<year>_<weekday><day>_<hour><min><sec>.jpg
+# Format: MM-DD-YY_DayDD_hhmmss
+# Example: 06-27-14_Thurs27_221834 => Thursday, June 27 2014 at 10:18pm
+#
 # External PiCam resources:
 # http://www.raspberrypi.org/documentation/usage/camera/python/README.md
 # http://www.raspberrypi.org/learning/python-picamera-setup/import time
 import time
+import datetime
 import picamera
 import RPi.GPIO as GPIO
 
@@ -16,12 +22,10 @@ MINUTE = 60 # in seconds
 HOUR = 3600 # in seconds
 
 camera = picamera.PiCamera()
-count = 0
 
 while (1): # run forever
-	count += 1
-	print 'capturing image', count
-	filename = '/media/usbhdd/img_%d.jpg' % count
+	date = datetime.datetime.now().strftime('%m-%d-%y_%a%b%d_%H%M%S')
+	filename = '/media/usbhdd/img_' + date + '.jpg'
+	print 'capturing image', date
 	camera.capture(filename)
 	time.sleep(DELAY) # capture every DELAY seconds
-
