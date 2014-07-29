9abb7098069f77c87c0f5a8e4fa07a173f470460
Justin <jcano001@ucr.edu>
2014-07-29 11:12:52 -0700

    added newline to serial data

--- uart-master.py
+++ uart-master.py
@@ -4,7 +4,7 @@ import light
 ser = serial.Serial('/dev/ttyAMA0', 9600)
 
 while True:
-	data = str(light.getLightReading())
+	data = str(light.getLightReading()) + '\n'
 	ser.write(data)
 	print data
 	time.sleep(3)
