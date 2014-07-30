1f866070ca98197dd36d0e1842255dfe4845befb
Justin <jcano001@ucr.edu>
2014-07-29 17:52:59 -0700

    Fetch sunrise/sunset times using pywapi

--- camcorder-day.py
+++ camcorder-day.py
@@ -18,40 +18,53 @@
 # http://www.raspberrypi.org/documentation/usage/camera/python/README.md
 # http://www.raspberrypi.org/learning/python-picamera-setup/
 # =====================================================================================
-import light
 import time
 import datetime
 import picamera
 import RPi.GPIO as GPIO
+import pywapi
 
-# The threshold value to determine when to turn on the camera. Range:
-# 1(light) ~ 50,000(dark)
-LIGHT_THRESHOLD = 20000
-OFFSET = 5000
+ZIP = "92507"
 
 MINUTE = 60 # in seconds
 HOUR = 3600 # in seconds
 
 RECORDING_LENGTH = HOUR # 3600 seconds
 
-camera = picamera.PiCamera()
-camera.resolution = (1920, 1080) # HD resolution
+#camera = picamera.PiCamera()
+#camera.resolution = (1920, 1080) # HD resolution
 
-def turnOnLED():
-	camera.led = True
+''' Get weather from different weather stations '''
+yahoo_weather = pywapi.get_weather_from_yahoo(ZIP)
+weather_com_weather = pywapi.get_weather_from_weather_com(ZIP)
 
-def turnOffLED():
-	camera.led = False
+sunrise = open('SUNRISE.txt', 'w+')
+sunset = open('SUNSET.txt', 'w+')
+if 'astronomy' in yahoo_weather:
+	if 'sunrise' in yahoo_weather['astronomy']:
+		sunrise.write(str(yahoo_weather['astronomy']['sunrise']))
+	if 'sunset' in yahoo_weather['astronomy']:
+		sunset.write(str(yahoo_weather['astronomy']['sunset']))
+
+
+elif 'forecasts' in weather_com_weather:
+	sunrise.write(str(weather_com_weather['forecasts'][0]['sunrise']))
+	sunset.write(str(weather_com_weather['forecasts'][0]['sunset']))
+sunrise.close()
+sunset.close()
+
+#def turnOnLED():
+#	camera.led = True
+
+#def turnOffLED():
+#	camera.led = False
 
 while (1): # run forever
-	reading = light.getLightReading()
-	if reading < LIGHT_THRESHOLD+OFFSET:
-		turnOnLED()
-		date = datetime.datetime.now().strftime('%m-%d-%y_%a%b%d_%H%M%S')
-		filename = '/media/usbhdd/video_' + date + '.h264'
-		print 'recording video clip', date
-		camera.start_recording(filename)
-		time.sleep(HOUR) # record for an hour
-		camera.stop_recording()
-	else:
-		turnOffLED()
+#	turnOnLED()
+	date = datetime.datetime.now().strftime('%m-%d-%y_%a%b%d_%H%M%S')
+	filename = '/media/usbhdd/video_' + date + '.h264'
+	print 'recording video clip', date
+#	camera.start_recording(filename)
+	time.sleep(HOUR) # record for an hour
+#	camera.stop_recording()
+#	turnOffLED()
