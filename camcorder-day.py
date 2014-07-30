dcc420ee7f56cd4b366e73c9460b065d18add782
Justin <jcano001@ucr.edu>
2014-07-29 19:48:32 -0700

    checks for daytime

--- camcorder-day.py
+++ camcorder-day.py
@@ -1,10 +1,12 @@
 # ===================================================================================== 
-# camcorder.py
+# camcorder-day.py
 # =====================================================================================    
 # Copyright 2014 Justin Cano
 # http://www.justincano.com
 #
 # Python script to record video clips to an external mounted USB NTFS hard drive.
+# This script is intended to operate the camera at DAY TIME hours, after sunrise and
+# before sunset.
 # Use time.sleep() record for desired amount of time
 #
 # Video files saved as .h264 named by their timestamp
@@ -34,6 +36,26 @@ RECORDING_LENGTH = HOUR # 3600 seconds
 
 SUNRISE = weather.SUNRISE
 SUNSET = weather.SUNSET
+SUNRISE_HOUR, SUNRISE_MINUTE = weather.parseTimestamp(SUNRISE)
+SUNSET_HOUR, SUNSET_MINUTE = weather.parseTimestamp(SUNSET)
+
+# <ON_HOUR:ON_MINUTE> = the time for the camera to turn on
+# Turns on an hour before sunrise
+ON_HOUR = SUNRISE_HOUR - 1
+ON_MINUTE = SUNRISE_MINUTE
+
+# <OFF_HOUR:OFF_MINUTE> = the time for the camera to turn off
+# Turns off an hour after sunset
+OFF_HOUR = SUNSET_HOUR + 1
+OFF_MINUTE = SUNSET_MINUTE
+
+def daytime(current_hour, current_minute):
+	if current_hour == ON_HOUR:
+		return current_minute >= ON_MINUTE
+	if current_hour == OFF_HOUR:
+		return current_minute < OFF_MINUTE
+	
+	return ON_HOUR < current_hour < OFF_HOUR
 
 #def turnOnLED():
 #	camera.led = True
@@ -43,16 +65,19 @@ SUNSET = weather.SUNSET
 
 def main():
 	date = datetime.datetime.now()
-	label = date.strftime('%m-%d-%y_%a%b%d_%H%M%S')
-	filename = '/media/usbhdd/video_' + label + '.h264'
-	sunrise_hour, sunrise_min = weather.parseTimestamp(SUNRISE)
-	print sunrise_hour, sunrise_min
-	time.sleep(10)
-#	print 'recording video clip', date
-#	camera.start_recording(filename)
-#	time.sleep(HOUR) # record for an hour
-#	camera.stop_recording()
-
+	current_hour = int(date.strftime('%H'))
+	current_minute = int(date.strftime('%M'))
+	if daytime(current_hour, current_minute):
+		print "daytime!"	
+		label = date.strftime('%m-%d-%y_%a%b%d_%H%M%S')
+		filename = '/media/usbhdd/video_' + label + '.h264'
+		time.sleep(10)
+#		print 'recording video clip', date
+#		camera.start_recording(filename)
+#		time.sleep(HOUR) # record for an hour
+#		camera.stop_recording()
+	else:
+		time.sleep(MINUTE)
 if __name__ == "__main__":
 	while True:
 		main()
